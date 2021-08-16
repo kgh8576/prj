c94a7d3f76d90ce97a218a8140bec1d185b732d8
GukJinSo <82632779+GukJinSo@users.noreply.github.com>
2021-08-16 14:19:35 +0900

    0816 크롤링 제발

--- styleconnect/src/main/java/com/one/style/crawldata/service/CrawlDataService.java
+++ styleconnect/src/main/java/com/one/style/crawldata/service/CrawlDataService.java
@@ -8,10 +8,10 @@ import com.one.style.crawldata.vo.CrawlDataVO;
 
 public interface CrawlDataService {
 	
-	 public void initCrawl(String gender , HttpServletRequest req);
+	 public void initCrawl(String gender);
 	 public void runCrawl();
 	 public void splitTest(String crawlText);
-	 public void start(String gender, HttpServletRequest req);
+	 public void start(String gender);
 	 void insertCrawl(CrawlDataVO vo);
 	 CrawlDataVO getCrawlList(String gender);
 	 List<CrawlDataVO> getCrawlData1Row();
