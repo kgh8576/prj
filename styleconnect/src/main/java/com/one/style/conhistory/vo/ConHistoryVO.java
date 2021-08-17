0898dd6071b6cd4cf6c303822c999b2a24e91123
admin <admin@YD02-19>
2021-08-17 14:26:33 +0900

    ccccc

--- styleconnect/src/main/java/com/one/style/conhistory/vo/ConHistoryVO.java
+++ styleconnect/src/main/java/com/one/style/conhistory/vo/ConHistoryVO.java
@@ -28,6 +28,7 @@ public class ConHistoryVO {
 	private String memName;
 	private String id; //디자이너 아이디
 	private int reviewexist; //리뷰 유무
+	private int canreview; //리뷰3일이내
 	
 	private String codecontent; //예약상황
 	private String major; //디자이너 전문분야
